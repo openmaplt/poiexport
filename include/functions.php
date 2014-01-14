f8aaba69620539383930e23a8b0a10b99ab44b99
Ramunas <ieskok@ramuno.lt>
2014-01-14 20:59:01 +0200

    switch to gettext translations

--- include/functions.php
+++ include/functions.php
@@ -1,54 +1,19 @@
 <?php
 /* globals */
-$VERSION = "v1.3";  // poi export version
-$LANG = "en";       // default language file is english
-$messages = null;   // array of message translations
-
-if(isset($_SESSION['LANG'])) {
-	$LANG = $_SESSION['LANG'];
-}
-
+define('VERSION', 'v1.3'); // poi export version
 
 /**
- *
- * Translate a text into the active language
- * @global string $LANG
- * @global string array $messages (see en.php for example)
- * @param string $s (message to look up and translate
- */
-function msg($s) {
-	global $LANG;
-	global $messages;
-
-	if (isset($messages[$s])) {
-		echo $messages[$s];
-	} else {
-		echo $s;
-	}
-}
-
-/**
- * include the required localization file
+ * Setup gettext translations
  */
 function i18n() {
-    global $LANG;
-	if (!isset($LANG) OR !isset($_SESSION['LANG'])) {
-		$LANG = split(",",$_SERVER["HTTP_ACCEPT_LANGUAGE"]);
-		$LANG = split("-",$LANG[0]);
-		$LANG = $LANG[0];
-		if ($LANG == "") {
-			$LANG = "en";
-		}
+	// Set language
+	$locale = \Locale::acceptFromHttp($_SERVER['HTTP_ACCEPT_LANGUAGE']);
+	if(!is_dir(__DIR__ . '/../locale/' . $locale)){
+		$locale = 'en_US';
 	}
-	global $messages;
-        
-	//if the language file does not exist, default to english
-    // The @ is used to suppress warnings if a localization file does not
-    // exist for the language
-	if (@include_once 'include/'.$LANG.'.php'){
-	} else {
-		$LANG = 'en';
-		include_once 'include/'.$LANG.'.php';
-	}
-	$_SESSION['LANG'] = $LANG;
+
+	\Locale::setDefault($locale);
+	putenv('LC_ALL=' . $locale);
+	setlocale(LC_ALL, $locale);
+	bindtextdomain('default', __DIR__ . '/../locale');
 }
--- index.php
+++ index.php
@@ -10,17 +10,17 @@ if(isset($_GET["l"])) {
 
 //Initialize localization
 i18n();
+textdomain('default');
 
-?>
-<!DOCTYPE html>
+?><!DOCTYPE html>
 <html>
     <head>
-        <title><? msg('OpenStreetMap POI export');?> - <? msg('download free point of interest'); ?></title>
+        <title><? echo _('OpenStreetMap POI Export');?> - <? echo _('download free point of interest'); ?></title>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <meta name="Description" content="<? msg('OpenStreetMap POI Export'); ?>" />
+        <meta name="Description" content="<? echo _('OpenStreetMap POI Export'); ?>" />
         <meta name="Keywords" content="openstreetmap,poi,waypoint,export,free,tomtom,garmin,google earth,oziexplorer,ov2,csv,gpx,wpt" />
         <meta name="robots" content="index, follow" />
-        <meta http-equiv="Content-Language" content="<? echo $_SESSION['LANG']; ?>" />
+        <meta http-equiv="Content-Language" content="<? echo Locale::getDefault(); ?>" />
         <link rel="shortcut icon" href="favicon.ico" />
         <link type="text/css" href="style.css" rel="stylesheet" />
         <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
@@ -30,106 +30,104 @@ i18n();
         <div class="wizardwrapper">
             <div class="wizardpanel 1">
                 <ul class="mainNav threeStep">
-                    <li class="current"><a title=""><em><? msg('1: POI type'); ?></em><span><? msg('Select the POI type') ?></span></a></li>
-                    <li><a title=""><em><? msg('2: Device'); ?></em><span><? msg('Select the device or file type you want to download to'); ?></span></a></li>
-                    <li class="mainNavNoBg"><a title=""><em><? msg('3: Export'); ?></em><span><? msg('Download POI file'); ?></span></a></li>
+                    <li class="current"><a title=""><em><? echo _('1: POI type'); ?></em><span><? echo _('Select the POI type') ?></span></a></li>
+                    <li><a title=""><em><? echo _('2: Device'); ?></em><span><? echo _('Select the device or file type you want to download to'); ?></span></a></li>
+                    <li class="mainNavNoBg"><a title=""><em><? echo _('3: Export'); ?></em><span><? echo _('Download POI file'); ?></span></a></li>
                 </ul>
                 <div class="wizardcontent">
                     <img src="images/pushpin.png" alt="" />
-                    <div class="explanation"><? msg('Select the type of POI you would like to download. Currently only one type can be selected.') ?></div>
+                    <div class="explanation"><? echo _('Select the type of POI you would like to download. Currently only one type can be selected.') ?></div>
                     <select name="select" multiple size="10" id="poitype" onDblclick="UpdateButtons(1);LoadNextPage(1,2);" onchange="UpdateButtons(1)">
-                        <option value="amenity:fuel"><? msg('Fuel'); ?></option>
-                        <option value="amenity:atm"><? msg('ATM'); ?></option>
-                        <option value="highway:speed_camera"><? msg('Speed camera'); ?></option>
-                        <option value="highway:bus_stop"><? msg('Bus stop'); ?></option>
-                        <option value="amenity:parking"><? msg('Parking'); ?></option>
-                        <option value="amenity:bicycle_parking"><? msg('Bicycle parking'); ?></option>
-                        <option value="amenity:place_of_worship"><? msg('Place of worship'); ?></option>
-                        <option value="amenity:hospital"><? msg('Hospital'); ?></option>
-                        <option value="shop:supermarket"><? msg('Supermarket'); ?></option>
-                        <option value="amenity:theatre"><? msg('Theatre'); ?></option>
-                        <option value="amenity:police"><? msg('Police'); ?></option>
-                        <option value="amenity:fire_station"><? msg('Fire station'); ?></option>
-                        <option value="amenity:post_box"><? msg('Post box'); ?></option>
-                        <option value="amenity:post_office"><? msg('Post office'); ?></option>
-                        <option value="amenity:recycling"><? msg('Recycling'); ?></option>
-                        <option value="amenity:restaurant"><? msg('Restaurant'); ?></option>
-                        <option value="amenity:fast_food"><? msg('Fast food'); ?></option>
-                        <option value="amenity:toilets"><? msg('Toilets'); ?></option>
-                        <option value="amenity:pub"><? msg('Pub'); ?></option>
-                        <option value="amenity:waste_basket"><? msg('Waste basket'); ?></option>
-						<option value="barrier:cattle_grid"><? msg('Cattle grid'); ?></option>
-                        <option value="tourism:camp_site"><? msg('Camp site'); ?></option>
-                        <option value="tourism:hotel"><? msg('Hotel'); ?></option>
-                        <option value="tourism:museum"><? msg('Museum'); ?></option>
-                        <option value="tourism:zoo"><? msg('Zoo'); ?></option>
-                        <option value="historic:castle"><? msg('Castle'); ?></option>
-                        <option value="man_made:windmill"><? msg('Windmill'); ?></option>
-                        <option value="man_made:lighthouse"><? msg('Lighthouse'); ?></option>
-                        <option value="man_made:watermill"><? msg('Watermill'); ?></option>
-                        <option value="man_made:water_tower"><? msg('Water tower'); ?></option>
-                        <option value="amenity:nightclub"><? msg('Nightclub'); ?></option>
-                        <option value="amenity:stripclub"><? msg('Stripclub'); ?></option>
+                        <option value="amenity:fuel"><? echo _('Fuel'); ?></option>
+                        <option value="amenity:atm"><? echo _('ATM'); ?></option>
+                        <option value="highway:speed_camera"><? echo _('Speed camera'); ?></option>
+                        <option value="highway:bus_stop"><? echo _('Bus stop'); ?></option>
+                        <option value="amenity:parking"><? echo _('Parking'); ?></option>
+                        <option value="amenity:bicycle_parking"><? echo _('Bicycle parking'); ?></option>
+                        <option value="amenity:place_of_worship"><? echo _('Place of worship'); ?></option>
+                        <option value="amenity:hospital"><? echo _('Hospital'); ?></option>
+                        <option value="shop:supermarket"><? echo _('Supermarket'); ?></option>
+                        <option value="amenity:theatre"><? echo _('Theatre'); ?></option>
+                        <option value="amenity:police"><? echo _('Police'); ?></option>
+                        <option value="amenity:fire_station"><? echo _('Fire station'); ?></option>
+                        <option value="amenity:post_box"><? echo _('Post box'); ?></option>
+                        <option value="amenity:post_office"><? echo _('Post office'); ?></option>
+                        <option value="amenity:recycling"><? echo _('Recycling'); ?></option>
+                        <option value="amenity:restaurant"><? echo _('Restaurant'); ?></option>
+                        <option value="amenity:fast_food"><? echo _('Fast food'); ?></option>
+                        <option value="amenity:toilets"><? echo _('Toilets'); ?></option>
+                        <option value="amenity:pub"><? echo _('Pub'); ?></option>
+                        <option value="amenity:waste_basket"><? echo _('Waste basket'); ?></option>
+                        <option value="barrier:cattle_grid"><? echo _('Cattle grid'); ?></option>
+                        <option value="tourism:camp_site"><? echo _('Camp site'); ?></option>
+                        <option value="tourism:hotel"><? echo _('Hotel'); ?></option>
+                        <option value="tourism:museum"><? echo _('Museum'); ?></option>
+                        <option value="tourism:zoo"><? echo _('Zoo'); ?></option>
+                        <option value="historic:castle"><? echo _('Castle'); ?></option>
+                        <option value="man_made:windmill"><? echo _('Windmill'); ?></option>
+                        <option value="man_made:lighthouse"><? echo _('Lighthouse'); ?></option>
+                        <option value="man_made:watermill"><? echo _('Watermill'); ?></option>
+                        <option value="man_made:water_tower"><? echo _('Water tower'); ?></option>
+                        <option value="amenity:nightclub"><? echo _('Nightclub'); ?></option>
+                        <option value="amenity:stripclub"><? echo _('Stripclub'); ?></option>
                     </select>
                 </div>
                 <div class="buttons">
-                    <button type="submit" id="next1" class="next" disabled="disabled" onclick="LoadNextPage(1,2);"><? msg('Next'); ?>&nbsp;&gt;&gt;</button>
+                    <button type="submit" id="next1" class="next" disabled="disabled" onclick="LoadNextPage(1,2);"><? echo _('Next'); ?>&nbsp;&gt;&gt;</button>
                 </div>
                 <div style="clear:both"></div>
             </div>
 
             <div class="wizardpanel 2">
                 <ul class="mainNav threeStep">
-                    <li class="lastDone"><a href="#" title="" onclick="LoadNextPage(2,1);"><em><? msg('1: POI type'); ?></em><span><? msg('Select the POI type') ?></span></a></li>
-                    <li class="current"><a title=""><em><? msg('2: Device'); ?></em><span><? msg('Select the device or file type you want to download to'); ?></span></a></li>
-                    <li class="mainNavNoBg"><a title=""><em><? msg('3: Export'); ?></em><span><? msg('Download POI file'); ?></span></a></li>
+                    <li class="lastDone"><a href="#" title="" onclick="LoadNextPage(2,1);"><em><? echo _('1: POI type'); ?></em><span><? echo _('Select the POI type') ?></span></a></li>
+                    <li class="current"><a title=""><em><? echo _('2: Device'); ?></em><span><? echo _('Select the device or file type you want to download to'); ?></span></a></li>
+                    <li class="mainNavNoBg"><a title=""><em><? echo _('3: Export'); ?></em><span><? echo _('Download POI file'); ?></span></a></li>
                 </ul>
                 <div class="wizardcontent">
                     <img src="images/device.png" alt="" />
-                    <div class="explanation"><? msg('Select the device or file type you want to download the POI in'); ?></div>
+                    <div class="explanation"><? echo _('Select the device or file type you want to download the POI in'); ?></div>
                     <select id="navitype" size="10"  onDblclick="UpdateButtons(2);LoadNextPage(2,3);" onchange="UpdateButtons(2)">
-                        <option value="ov2"><? msg('TomTom overlay (ov2)'); ?></option>
-                        <option value="csv"><? msg('Garmin (csv)'); ?></option>
-                        <option value="gpx"><? msg('GPS Exchange format (gpx)'); ?></option>
-                        <option value="kml"><? msg('Google Earth (kml)'); ?></option>
-			<option value="wpt"><? msg('OziExplorer (wpt)'); ?></option>
-			<option value="geojson"><? msg('GeoJSON (geojson)'); ?></option>
-                        <!--<option value="osm"><? msg('OpenStreetMap (osm)'); ?></option>-->
+                        <option value="ov2"><? echo _('TomTom overlay (ov2)'); ?></option>
+                        <option value="csv"><? echo _('Garmin (csv)'); ?></option>
+                        <option value="gpx"><? echo _('GPS Exchange format (gpx)'); ?></option>
+                        <option value="kml"><? echo _('Google Earth (kml)'); ?></option>
+						<option value="wpt"><? echo _('OziExplorer (wpt)'); ?></option>
+						<option value="geojson"><? echo _('GeoJSON (geojson)'); ?></option>
                     </select>
                 </div>
                 <div class="buttons">
-                    <button type="submit" id="back2" class="previous" onclick="LoadNextPage(2,1);">&lt;&lt;&nbsp;<? msg('Previous'); ?></button>
-                    <button type="submit" id="next2" class="next" disabled="disabled" onclick="LoadNextPage(2,3);"><? msg('Next'); ?>&nbsp;&gt;&gt;</button>
+                    <button type="submit" id="back2" class="previous" onclick="LoadNextPage(2,1);">&lt;&lt;&nbsp;<? echo _('Previous'); ?></button>
+                    <button type="submit" id="next2" class="next" disabled="disabled" onclick="LoadNextPage(2,3);"><? echo _('Next'); ?>&nbsp;&gt;&gt;</button>
                 </div>
-
                 <div style="clear:both"></div>
             </div>
 
 
             <div class="wizardpanel 3">
                 <ul class="mainNav threeStep">
-                    <li class="done"><a href="#" title="" onclick="LoadNextPage(3,1);"><em><? msg('1: POI type'); ?></em><span><? msg('Select the POI type') ?></span></a></li>
-                    <li class="lastDone"><a href="#" onclick="LoadNextPage(3,2);" title=""><em><? msg('2: Device'); ?></em><span><? msg('Select the device or file type you want to download to'); ?></span></a></li>
-                    <li class="mainNavNoBg current"><a title=""><em><? msg('3: Export'); ?></em><span><? msg('Download POI file'); ?></span></a></li>
+                    <li class="done"><a href="#" title="" onclick="LoadNextPage(3,1);"><em><? echo _('Step 1: POI type'); ?></em><span><? echo _('Select the POI type') ?></span></a></li>
+                    <li class="lastDone"><a href="#" onclick="LoadNextPage(3,2);" title=""><em><? echo _('Step 2: Select device'); ?></em><span><? echo _('Select the device or file type you want to download to'); ?></span></a></li>
+                    <li class="mainNavNoBg current"><a title=""><em><? echo _('Step 3: Download'); ?></em><span><? echo _('Download POI file'); ?></span></a></li>
                 </ul>
                 <div class="wizardcontent">
                     <img src="images/download.png" alt="" />
-                    <div class="explanation"><? msg('The file is ready for download.') ?></div>
+                    <div class="explanation"><? echo _('The file is ready for download.') ?></div>
                     <div id="action">
-                        <p><? msg('Poi type') ?> : - </p>
-                        <p><? msg('Device type') ?> : -</p>
+                        <p><? echo _('Poi type') ?> : - </p>
+                        <p><? echo _('Device type') ?> : -</p>
                     </div>
                 </div>
                 <div class="buttons">
-                    <button type="submit" id="back3" class="previous" onclick="LoadNextPage(3,2);">&lt;&lt;&nbsp;<? msg('Previous'); ?></button>
-                    <button type="submit" id="next3" class="next"  onclick="DownloadFile();"><? msg('Download'); ?></button>
+                    <button type="submit" id="back3" class="previous" onclick="LoadNextPage(3,2);">&lt;&lt;&nbsp;<? echo _('Previous'); ?></button>
+                    <button type="submit" id="next3" class="next"  onclick="DownloadFile();"><? echo _('Download'); ?></button>
                 </div>
                 <div style="clear:both"></div>
             </div>
-            <div class="footer"><? msg('Poi Export'); ?> - <? echo $VERSION; ?>&nbsp;
-                <? msg('data'); ?> <a href="http://creativecommons.org/licenses/by-sa/2.0/">cc-by-sa</a>&nbsp;
-                <a href="http://www.openstreetmap.nl/"><? msg('OpenStreetMap'); ?></a>&nbsp;<? msg('community') ?>.&nbsp;-&nbsp;
-                <? msg('Created by'); ?>&nbsp;<a href="http://www.openstreetmap.org/user/rullzer">rullzer</a>&nbsp;<? msg('and'); ?>&nbsp;<a href="http://www.openstreetmap.org/user/Rubke">rubke</a>
+            <div class="footer"><? echo _('Poi Export'); ?> - <? echo VERSION; ?>&nbsp;
+                <? echo _('data'); ?> <a href="http://creativecommons.org/licenses/by-sa/2.0/">cc-by-sa</a>&nbsp;
+                <a href="http://www.openstreetmap.nl/"><? echo _('OpenStreetMap'); ?></a>&nbsp;<? echo _('community') ?>.&nbsp;-&nbsp;
+                <? echo _('Created by'); ?>&nbsp;<a href="http://www.openstreetmap.org/user/rullzer">rullzer</a>&nbsp;<? echo _('and'); ?>&nbsp;<a href="http://www.openstreetmap.org/user/Rubke">rubke</a>
            </div>
         </div>
     </body>
